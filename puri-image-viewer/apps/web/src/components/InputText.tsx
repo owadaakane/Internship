e11518075630690fd5fd19536bdde3d09c899485
owada.akane <owada.akane@furyu.jp>
2024-09-24 16:38:42 +0900

    JSDoc追加

--- puri-image-viewer/apps/web/src/components/InputText.tsx
+++ puri-image-viewer/apps/web/src/components/InputText.tsx
@@ -1,3 +1,16 @@
+/**
+ * ラベル付きの入力フィールドをレンダリングする。
+ *
+ * @component
+ * @param {Object} props - コンポーネントのプロパティ。
+ * @param {string} props.title - 入力フィールドのラベルテキスト。
+ * @param {string} props.id - 入力要素のid属性。
+ * @param {string} props.type - 入力要素のtype属性。
+ * @param {string} props.placeholder - 入力要素のプレースホルダーテキスト。
+ * @param {function} props.onChange - 入力変更を処理するコールバック関数。
+ * @returns {JSX.Element} レンダリングされたInputTextコンポーネント。
+ */
+
 type Props = {
   title: string;
   id: string;
